eccef55edac115b8fa7d671851c8ec1a13e9fe3f
GoldieAi <102845355+AE-001-1001@users.noreply.github.com>
2022-08-28 18:37:20 -0500

    branch change

---
+++ .history/branch_20220828142258.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <ctype.h>
+
+using namespace std;
+
+
+
+int stream(int a){
+    for (int i=0; i<a; i++){
+    if (a >= 12){
+            a++;
+    }
+    if (a > 1337){
+        a = a--;
+        i = a/100;
+    }
+        cout << a << "\n" <<  i << "\n";
+    }
+    if (a == EOF)
+        return EOF;
+    else
+        return 0;
+};
+
+float strf(float k){
+    for (float i=0; i<k; i++){
+        if (k == i) {
+            i++;
+        }
+        else
+        if (k > 100.525){
+            k = k--;
+        }
+    cout << i << "\t" << k << "\n";
+    return 0;
+}};
+
+int main()
+{
+    stream(2004);
+    strf(2004.9050);
+    return stream(15);
+};
+
+
