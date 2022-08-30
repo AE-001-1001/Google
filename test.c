443223f917cbfbd54704a44af3217466bd710ea5
GoldieAi <102845355+AE-001-1001@users.noreply.github.com>
2022-08-30 12:42:34 -0500

    Committing the Untracked Files

--- test.c
+++ test.c
@@ -3,27 +3,32 @@
 #include <stdarg.h>
 
 
-void number(int x)
+void number(int x,int)
 {
-    printf("Number of elements: %d\n", (int) (sizeof(int) * x));
+    printf("Number of elements: %d\n", (int) (sizeof(int) + x));
 }
 
-
+// Creates a string representation of Numbers given by user
 int num_processor(int x, int size)
 {
     for (int x = 0; x < size; x++)
     {
+        if (x % 5 == 0)
+        {
+            number(x,size);
+        }
         printf("%d\t", size-x);
     }
-    return 0;
+    return 1;
 }
-
+//End
 
 int main()
 {
 int Mynum;
+int num;
 printf("Type a number: \n");
 scanf_s("%d", &Mynum);
 printf("Your number is: %d\n", Mynum);
-num_processor(1, Mynum);
+num_processor(num, Mynum);
 }
