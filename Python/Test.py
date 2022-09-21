26debd2497f088860f2eb31a544baea84a06cbfa
A-115 <102845355+AE-001-1001@users.noreply.github.com>
2022-09-21 10:48:52 -0500

    1. Added Python to Project 2. Made a Different html 3. Learning Typescript instead of javascript

--- Python/Test.py
+++ Python/Test.py
@@ -1,10 +1,10 @@
 from ssl import OPENSSL_VERSION
 import requests 
 # Use OPENSSL functions to create connections
-def create_connection(w):
+def create_connection(w,y):
     VERSION = OPENSSL_VERSION
-    test = requests.post(w, 'https://www.robinhood.com',headers={'Content-Type': 'application/json'})
+    test = requests.post(w, y,headers={'Content-Type': 'application/json'})
     data = [("Creating SSL connection: %s" % VERSION), ("SSL connection established: %s" % test)]
     return print(data, sep='\n',end='\n')
 
-create_connection("https://www.robinhood.com")
+create_connection("https://www.robinhood.com","https://www.google.com")
