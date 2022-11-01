18459fd5b89bce6790642c18e39c6094873ebb27
A-115 <102845355+AE-001-1001@users.noreply.github.com>
2022-11-01 02:15:38 -0500

    dll's and stuffis for buildus

--- C#/app/test.cs
+++ C#/app/test.cs
@@ -3,9 +3,24 @@ namespace helloWorld
 {
     class tester
     {
+        public static float Maths(int a, int b)
+        {
+            // check if numbers are greater or equal than eachother
+            if (a <= b)
+            {
+                // return the difference
+                return b - a * 3.14f+ DateTime.Compare(DateTime.Now, DateTime.Now);
+            }
+            else
+            {
+                // return the sum
+                return a + b * 3.14f;
+            }
+        }
         public static void test()
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Maths(9, 10));
         }
     }
 }
